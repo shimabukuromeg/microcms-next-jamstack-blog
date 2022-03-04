1e12b9fb44506dbfbbbb5ff11da0c35aa8ef2ca1
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-03-04 22:34:24 +0900

    プレビューモードを終了する処理追加

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import { HeadTemplate } from '../../components/HeadTemplate';
 import type { Blog } from '../index';
 import ErrorPage from 'next/error';
+import { Link as NextUiLink } from '@nextui-org/react';
 
 const Blog: NextPage<{
   blog: Blog;
@@ -27,19 +28,18 @@ const Blog: NextPage<{
         pagepath="blogs"
         postimg={ogImageUrl}
       />
-      {
-        preview && (
-          <Container>
+      {preview && (
+        <Container>
           <Card color="gradient">
             <Row justify="center" align="center">
               <Text h6 size={15} color="white" css={{ m: 0 }}>
-                下書きのプレビューです
+                下書きのプレビューです。プレビューを終了する場合は
+                <NextUiLink block color="error" href="/api/exit-preview">こちら</NextUiLink>
               </Text>
             </Row>
           </Card>
         </Container>
-        )
-      }
+      )}
       <Container css={{ p: 30 }}>
         <h1>{blog.title}</h1>
         <p>{blog.publishedAt}</p>
@@ -81,7 +81,7 @@ export const getStaticPaths = async () => {
   return { paths, fallback: true };
 };
 
-export const getStaticProps = async (context: any) => {  
+export const getStaticProps = async (context: any) => {
   const slug = context.params?.slug;
   const draftKey = context.previewData?.draftKey;
   const blog = await fetch(
